47d4d24e82fe4822f0c5b723e3b30e2114c2eb1d
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-03-22 11:25:52 +0000

    add Toggle temperator scale button

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # weather-app
+
+Photo by <a href="https://unsplash.com/@nedaastani?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Neda Astani</a> on <a href="https://unsplash.com/photos/green-grass-field-near-lake-under-white-clouds-and-blue-sky-during-daytime-KWTkd7mHqKE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const searchInput = document.querySelector("#search");
 const getImageButton = document.querySelector(".getImageButton");
+const todaysWeather = document.querySelector(".todaysWeather");
+todaysWeather.style.display = "none";
 
+const toggleScale = document.querySelector(".toggleScale");
+
+toggleScale.addEventListener('click', () => {
+  if (toggleScale.textContent === "Fahrenheit") {
+    toggleScale.textContent = "Celcius";
+  } else {
+    toggleScale.textContent = "Fahrenheit";
+  }
+});
 getImageButton.addEventListener("click", (e) => {
   e.preventDefault();
+  fetchWeatherData();
+});
+
+const fetchWeatherData = () => {
   const searchValue = searchInput.value;
   searchInput.value = "";
   const apiKey = "BDNZ6FSQE7KL9EFRRJ9NEKYX2";
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchValue}?key=${apiKey}`;
+  let scale = "unitGroup=us";
+  if (toggleScale.textContent === "Celcius") {
+    scale = "unitGroup=metric";
+  }
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchValue}?${scale}&key=${apiKey}&contentType=json`;
   fetch(
     url,
     { mode: "cors" }
@@ -34,14 +53,39 @@ getImageButton.addEventListener("click", (e) => {
     const rightPart = document.querySelector(".rightPart");
     rightPart.innerHTML = `
       <p>Feelslike: ${day[0].feelslike}</p>
-      <p>${day[0].tempmax}  ${day[0].tempmin}</p>
+      <p>max:${day[0].tempmax}    min:${day[0].tempmin}</p>
       <p>Windspeed: ${day[0].windspeed}</p>
       <p>Humidity: ${day[0].humidity}</p>
     `
 
-    
+    console.log(day[0].icon);
+    const gifURL = `https://api.giphy.com/v1/gifs/translate?api_key=NXnIWDAYjvO47OEb58eZBQqRdBvfhMNv&s=${day[0].icon}`;
+
+    fetch(
+      gifURL,
+      { mode: "cors" }
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("gif response not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (!data) {
+          console.log("Empty data array");
+        }
+        const imgSection = document.querySelector(".gif");
+        imgSection.src = data.data.images.original.url;
+        console.log(data);
+        console.log(data.data);
+        console.log(data.data.images.original.url);
+      })
+      .catch(error => {
+        console.error(`Gif Fetch error: ${error}`);
+      })
   })
   .catch(error => {
-    console.error("fetch error");
+    console.error(`fetch error: ${error}`);
   });
-});
+};
